4bd1e533f08b2ab5383124bf1fd98547d99cd2ab
miintTeaa <89668833+miintTeaa@users.noreply.github.com>
2023-06-02 03:52:30 +0100

    removed unincluded visitor module

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,7 +1,6 @@
 mod expr;
 pub mod ops;
 mod stmt;
-// pub mod visitor;
 
 pub use expr::*;
 pub use stmt::*;
--- src/ast/visitor/mod.rs
+++
@@ -1,5 +0,0 @@
-mod expr;
-mod stmt;
-
-pub use expr::ExprVisitor;
-pub use stmt::StmtVisitor;
